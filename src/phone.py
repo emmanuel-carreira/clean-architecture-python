983fc56e95a185f7d51371f2236663a3297d066f
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-11-23 00:51:07 +0000

    feat: adds phone value object

---
+++ src/phone.py
@@ -0,0 +1,14 @@
+import re
+
+class Phone:
+    DDD_PATTERN = r"^\d{2}$"
+    NUMBER_PATTERN = r"^\d{8,9}$"
+
+    def __init__(self, ddd: str, number: str) -> None:
+        if ddd is None or not re.match(self.DDD_PATTERN, ddd):
+            raise ValueError("Invalid DDD!")
+        if number is None or not re.match(self.NUMBER_PATTERN, number):
+            raise ValueError("Invalid number!")
+
+        self.ddd = ddd
+        self.number = number
---
+++ tests/unit/test_phone.py
@@ -0,0 +1,38 @@
+import unittest
+
+from src.phone import Phone
+
+
+class PhoneTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.valid_ddd = "99"
+        self.valid_number = "999999999"
+
+    def test_should_not_create_phone_with_none_ddd(self):
+        with self.assertRaises(ValueError):
+            Phone(None, self.valid_number)
+
+    def test_should_not_create_phone_with_empty_ddd(self):
+        with self.assertRaises(ValueError):
+            Phone("", self.valid_number)
+
+    def test_should_not_create_phone_with_invalid_ddd(self):
+        with self.assertRaises(ValueError):
+            Phone("(99)", self.valid_number)
+
+    def test_should_not_create_phone_with_none_number(self):
+        with self.assertRaises(ValueError):
+            Phone(self.valid_ddd, None)
+
+    def test_should_not_create_phone_with_empty_number(self):
+        with self.assertRaises(ValueError):
+            Phone(self.valid_ddd, "")
+
+    def test_should_not_create_phone_with_invalid_number(self):
+        with self.assertRaises(ValueError):
+            Phone(self.valid_ddd, "9999-9999")
+
+    def test_should_create_phone_with_valid_ddd_and_number(self):
+        phone = Phone(self.valid_ddd, self.valid_number)
+        self.assertEqual(phone.ddd, self.valid_ddd)
+        self.assertEqual(phone.number, self.valid_number)
