bbb14dcc82c1aaa5492a23b26270204397c1538b
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-11-25 20:56:19 +0000

    feat: adds InMemoryStudentRepository as an example of StudentRepository

---
+++ school/domain/student/student_not_found.py
@@ -0,0 +1,11 @@
+from school.domain.cpf import CPF
+
+
+class StudentNotFound(Exception):
+    def __init__(self, cpf: CPF, *args) -> None:
+        super().__init__(cpf, *args)
+        self.cpf = cpf
+
+    def __str__(self) -> str:
+        return f"Student with CPF {self.cpf} not found!"
+        
