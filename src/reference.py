8fa1f9dec0a2811fbc5be1c797fe49874ade7f6a
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-11-25 02:04:09 +0000

    feat: adds reference value object

---
+++ src/reference.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from src.student import Student
+
+
+class Reference:
+    def __init__(self, indicated_student: Student, indicator_student: Student) -> None:
+        self.indicated_student = indicated_student
+        self.indicator_student = indicator_student
+        self.reference_date = datetime.utcnow()
