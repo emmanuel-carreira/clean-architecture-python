784d5476d9e8b5c181059287e8c808c1948fea58
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-11-26 20:26:25 +0000

    feat: adds send referenced email service

---
+++ school/application/reference/send_referenced_email_service.py
@@ -0,0 +1,6 @@
+from school.domain.student.student import Student
+
+
+class SendReferencedEmailService:
+    def send_to(self, indicated_student: Student) -> None:
+        raise NotImplementedError
