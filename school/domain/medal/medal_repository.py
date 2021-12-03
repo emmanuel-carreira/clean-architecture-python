463b6b730035e52cc1e551085c1bb4e61279da69
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-12-03 22:25:32 +0000

    feat: adds InMemoryMedalRepository

--- school/domain/medal/medal_repository.py
+++ school/domain/medal/medal_repository.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from school.domain.cpf import CPF
 from school.domain.medal.medal import Medal
 
 
 class MedalRepository:
-    def add_medal(self, medal: Medal):
+    def add_medal(self, medal: Medal) -> None:
         raise NotImplementedError
 
-    def list_student_medals(self, student_cpf: CPF):
+    def list_student_medals(self, student_cpf: CPF) -> List[Medal]:
         raise NotImplementedError
