6554c2d02eeec603245b886753aa0912a68d5706
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-12-03 01:51:17 +0000

    refactor: updates student register use case to publish an registered student event

--- school/application/student/register_student.py
+++ school/application/student/register_student.py
@@ -1,11 +1,15 @@
 from school.application.student.register_student_dto import RegisterStudentDTO
+from school.domain.event_publisher import EventPublisher
+from school.domain.student.registered_student_event import RegisteredStudentEvent
 from school.domain.student.student_repository import StudentRepository
 
 
 class RegisterStudent:
-    def __init__(self, repository: StudentRepository) -> None:
+    def __init__(self, repository: StudentRepository, publisher: EventPublisher) -> None:
         self.repository = repository
+        self.publisher = publisher
 
     def execute(self, student_data: RegisterStudentDTO) -> None:
         student = student_data.create_student()
         self.repository.register(student)
+        self.publisher.publish(RegisteredStudentEvent(student.cpf))
--- tests/unit/test_register_student.py
+++ tests/unit/test_register_student.py
@@ -3,16 +3,18 @@ import unittest
 from school.application.student.register_student import RegisterStudent
 from school.application.student.register_student_dto import RegisterStudentDTO
 from school.domain.cpf import CPF
+from school.domain.event_publisher import EventPublisher
 from school.infrastructure.student.in_memory_student_repository import InMemoryStudentRepository
 
 
 class RegisterStudentTest(unittest.TestCase):
     def test_student_should_be_persisted(self):
+        publisher = EventPublisher()
         repository = InMemoryStudentRepository()
         self.assertEqual(repository.list_all_registered_students(), [])
 
         student_data = RegisterStudentDTO(cpf="123.456.789-00", email="test@test.com", name="Test")
-        register_student = RegisterStudent(repository)
+        register_student = RegisterStudent(repository, publisher)
         register_student.execute(student_data)
 
         self.assertIsNotNone(repository.search_by_cpf(CPF("123.456.789-00")))
